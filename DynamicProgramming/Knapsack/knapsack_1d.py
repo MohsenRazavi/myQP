e04904c1f034744f9b8c6d1bb438f8447f96725b
MohsenRazavi <mohsenrazavi.self@gmail.com>
2024-05-08 12:07:33 +0330

    fix knapsack

--- DynamicProgramming/Knapsack/knapsack_1d.py
+++ DynamicProgramming/Knapsack/knapsack_1d.py
@@ -5,9 +5,11 @@ weights = [int(i) for i in input().split()]
 n = len(weights)
 m = sum(weights)
 
-possible = [False for _ in range(m)]
+possible = [False for _ in range(m+1)]
 possible[0] = True
 
-for k in range(n):
+for k in range(n+1):
     for x in range(m-weights[k], 0, -1):
         possible[x+weights[k]] |= possible[x]
+
+#TODO This code needs to be fixed !
--- DynamicProgramming/Knapsack/knapsack_2d.py
+++ DynamicProgramming/Knapsack/knapsack_2d.py
@@ -1,22 +1,25 @@
-
 """
 weights is array of weights
 k is number of items (length of weight)
 we will construct a 2D list named possible[k][x] which says that we can make sum of x by using k first weights or not
 """
 
+
+def check_possibility():
+    for k in range(1, n + 1):
+        for x in range(m + 1):
+            if x - weights[k - 1] >= 0:
+                possible[k][x] |= possible[k - 1][x - weights[k - 1]]
+            possible[k][x] |= possible[k - 1][x]
+    return possible[n][m]
+
+
 target_sum = int(input())
 weights = [int(i) for i in input().split()]
 n = len(weights)
 m = sum(weights)
 
-possible = [[False for _ in range(m)] for _ in range(k)]
+possible = [[False for _ in range(m + 1)] for _ in range(n + 1)]
 possible[0][0] = True
 
-for k in range(1, n+1):
-    for x in range(m+1):
-        if x-weights[k] >= 0:
-            possible[k][x] |= possible[k-1][x-weights[k]]
-        possible[k][x] |= possible[k-1][x]
-
-
+check_possibility()
